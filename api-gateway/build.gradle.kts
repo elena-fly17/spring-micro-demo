96eb8f34e32dbb52f5cc5934d774909f8e1ab74d
elena-fly17 <elena17mr@gmail.com>
2023-04-20 18:20:53 +0300

    Добавлен шлюз API Gateway

---
+++ api-gateway/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    java
+    id("org.springframework.boot") version "2.7.11"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
+}
+
+group = "ru.javabegin.micro.demo"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+    mavenCentral()
+}
+
+extra["springCloudVersion"] = "2021.0.6"
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
